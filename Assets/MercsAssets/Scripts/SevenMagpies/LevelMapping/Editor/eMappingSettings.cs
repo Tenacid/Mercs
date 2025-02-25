3c035db521d63fe4180981880bc29cdbf4029ef7
Evgeniy Mitin <evgeniy.mitin@gmail.com>
2025-02-25 23:29:57 +0100

    LevelData

--- Assets/MercsAssets/Scripts/SevenMagpies/LevelMapping/Editor/eMappingSettings.cs
+++ Assets/MercsAssets/Scripts/SevenMagpies/LevelMapping/Editor/eMappingSettings.cs
@@ -8,6 +8,7 @@ using SevenMagpies.LevelMapping;
 public class eMappingSettings : Editor
 {
     private static string LastPath;
+    private static string LevelDataPath;
 
     public override void OnInspectorGUI()
     {
@@ -16,6 +17,11 @@ public class eMappingSettings : Editor
         if ( GUILayout.Button( "Import Mapping Meta from file" ) )
         {
             ImportMappingMeta();
+        }
+
+        if ( GUILayout.Button( "Import Level" ) )
+        {
+            ImportLevel();
         }
     }
 
@@ -36,5 +42,45 @@ public class eMappingSettings : Editor
         LastPath = System.IO.Path.GetFullPath( filePath );
 
         MappingImporter.ImportMappingMeta( filePath, ( MappingSettings ) target );
+    }
+
+    private void ImportLevel() 
+    {
+        if ( string.IsNullOrEmpty( LastPath ) )
+        {
+            LastPath = Application.dataPath;
+        }
+
+        var filePath = EditorUtility.OpenFilePanel( "Select png file with level", LastPath, "png" );
+
+        if ( string.IsNullOrEmpty( filePath ) )
+        {
+            return;
+        }
+
+        LastPath = System.IO.Path.GetFullPath( filePath );
+
+        var data = MappingImporter.ImportLevel( filePath, ( MappingSettings ) target );
+
+        var levelData = ScriptableObject.CreateInstance<LevelData>();
+        levelData.FillData( data );
+
+        if ( string.IsNullOrEmpty( LevelDataPath ) )
+        {
+            LevelDataPath = Application.dataPath;
+        }
+
+        var savePath = EditorUtility.SaveFilePanel( "Save level", LevelDataPath, System.IO.Path.GetFileNameWithoutExtension( filePath ), "asset" );
+        var appPath = Application.dataPath;
+
+        if ( string.IsNullOrEmpty( savePath ) )
+        {
+            return;
+        }
+
+        LevelDataPath = System.IO.Path.GetFullPath( savePath );
+
+        UnityEditor.AssetDatabase.CreateAsset( levelData, savePath.Replace(appPath, "Assets" ) );
+
     }
 }
---
+++ Assets/MercsAssets/Scripts/SevenMagpies/LevelMapping/LevelData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SevenMagpies.LevelMapping
+{
+    public class LevelData : ScriptableObject
+    {
+        [SerializeField]
+        private int _width;
+
+        [SerializeField]
+        private int _height;
+
+        [SerializeField]
+        private List<string> _data;
+
+        
+        
+
+        public void FillData( string[,] incomingData )
+        {
+            _width = incomingData.GetLength( 0 );
+            _height = incomingData.GetLength( 1 );
+
+            _data = new List<string>();
+            for ( var x = 0; x < incomingData.GetLength( 0 ); x++ )
+            {
+                for ( var y = 0; y < incomingData.GetLength( 1 ); y++ )
+                {
+                    _data.Add( incomingData[ x, y ] );
+                }
+            }
+        }
+    }
+}
